85a34d9720211eec26ca1450a5aca8aa05ee3a17
Den <dearian2706@gmail.com>
2023-04-25 14:04:38 +0300

    change port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://dearian27.github.io/Youtube_app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
