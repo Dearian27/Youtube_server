7a8f3a7b830049933ea9ab443046d94777f53f0b
Den <dearian2706@gmail.com>
2023-04-30 22:27:30 +0300

    set one cors origin

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: [process.env.CORS_ORIGIN,process.env.CORS_ORIGIN_LOCAL],
+  // origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN_LOCAL],
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
