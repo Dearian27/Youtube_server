c39597eb03e95acf2daa6b61792c1baeb74b5472
Den <dearian2706@gmail.com>
2023-05-06 23:32:23 +0300

    user properties check

--- controllers/video.js
+++ controllers/video.js
@@ -74,7 +74,7 @@ export const addView = async (req, res, next) => {
   }
 }
 export const getPending = async (req, res, next) => {
-  if (!req.user.isAdmin) {
+  if (!req.user?.isAdmin) {
     res.status(403).json("You don't have permission");
   }
   try {
