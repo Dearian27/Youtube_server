f08d253a915697cd6de4017585b78eaf40c419fe
Den <dearian2706@gmail.com>
2023-05-06 23:35:56 +0300

    add user check

--- controllers/video.js
+++ controllers/video.js
@@ -74,7 +74,12 @@ export const addView = async (req, res, next) => {
   }
 }
 export const getPending = async (req, res, next) => {
-  if (!req.user?.isAdmin) {
+  if(req.user) {
+    if (!req.user.isAdmin) {
+      res.status(403).json("You don't have permission");
+    }  
+  }
+  else {
     res.status(403).json("You don't have permission");
   }
   try {
