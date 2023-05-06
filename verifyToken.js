b9abd76441a29e1a036a661316a573f7ffa316ae
Den <dearian2706@gmail.com>
2023-05-07 00:11:04 +0300

    find user in approving

--- verifyToken.js
+++ verifyToken.js
@@ -3,9 +3,10 @@ import { createError } from './error.js';
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
     if (!token) return next(createError(401, "You are not authenticated"));
-  
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) return next(createError(403, "Token verification failed"));
+    console.log('token', token)
+    console.log('user', user)
     req.user = user;
     next();
   })
