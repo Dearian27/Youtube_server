c10ac735d1878bee6dcb6c256c2d6820d4ee431c
Den <dearian2706@gmail.com>
2023-05-09 23:40:04 +0300

    add user token log

--- verifyToken.js
+++ verifyToken.js
@@ -5,6 +5,7 @@ export const verifyToken = (req, res, next) => {
     if (!token) return next(createError(401, "You are not authenticated"));
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) return next(createError(403, "Token verification failed"));
+    console.log("user", user);
     req.user = user;
     next();
   })
