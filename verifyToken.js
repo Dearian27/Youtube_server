ec82350bc7833608e20de5d2d40464bc2e226008
Den <dearian2706@gmail.com>
2023-05-22 18:27:08 +0300

    add logs

--- verifyToken.js
+++ verifyToken.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken';
 import { createError } from './error.js';
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
+    console.log("kakish", req.cookies);
+    console.log("kakish", req.cookies.access_token);
     if (!token) return next(createError(401, "You are not authenticated"));
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) return next(createError(403, "Token verification failed"));
