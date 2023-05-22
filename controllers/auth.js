86f022051823c58f2cdc4b0460a39316c013abf3
Den <dearian2706@gmail.com>
2023-05-22 18:59:56 +0300

    change SSCP

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,7 +50,7 @@ export const signIn = async (req, res, next) => {
     const { userPassword, ...other } = user._doc;
     res.cookie("access_token", token, {
       httpOnly: true,
-      sameSite: 'none',
+      sameSite: 'lax',
       secure: true
     }).status(200).json(other);
   } catch (error) {
@@ -65,7 +65,7 @@ export const signInGoogle = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
       return res.cookie("access_token", token, {
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: 'lax',
         secure: true
       })
       .status(200)
