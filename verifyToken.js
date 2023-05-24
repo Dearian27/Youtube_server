ea77ca33054b07023bbe61e6ccb6f7b4904faee6
Den <dearian2706@gmail.com>
2023-05-24 20:04:52 +0300

    add logs

--- verifyToken.js
+++ verifyToken.js
@@ -4,8 +4,9 @@ export const verifyToken = (req, res, next) => {
     const token = (req.headers.authorization || '').replace(/Bearer\s?/, '')
     if (!token) return next(createError(401, "You are not authenticated"));
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-    if (err) return next(createError(403, "Token verification failed"));
-    console.log('before req.user')
+      console.log('before 403')
+      if (err) return next(createError(403, "Token verification failed"));
+      console.log('before req.user')
     req.user = user;
     console.log('after req.user')
     next();
