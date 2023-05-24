d495e4099a398ad5c195abd9b63b17e89ae2a9d6
Den <dearian2706@gmail.com>
2023-05-24 19:11:33 +0300

    change token origin

--- verifyToken.js
+++ verifyToken.js
@@ -1,13 +1,10 @@
 import jwt from 'jsonwebtoken';
 import { createError } from './error.js';
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
-    console.log("kakish", req.cookies);
-    console.log("kakish", req.cookies.access_token);
+    const token = (req.headers.authorization || '').replace(/Bearer\s?/, '')
     if (!token) return next(createError(401, "You are not authenticated"));
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) return next(createError(403, "Token verification failed"));
-    console.log("user", user);
     req.user = user;
     next();
   })
